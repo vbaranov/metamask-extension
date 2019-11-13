fc4af180b0e057abf85c68ed5b08e1838d614529
Erik Marks <rekmarks@protonmail.com>
2019-11-13 10:12:28 -0800

    update metamask-inpage-provider

--- test/e2e/ethereum-on.spec.js
+++ test/e2e/ethereum-on.spec.js
@@ -133,7 +133,7 @@ describe('MetaMask', function () {
     })
 
     it('has not set the network within the dapp', async () => {
-      const networkDiv = await findElement(driver, By.css('#network'))
+      const networkDiv = await findElement(driver, By.css('#networkDiv'))
       assert.equal(await networkDiv.getText(), '')
     })
 
@@ -151,19 +151,19 @@ describe('MetaMask', function () {
 
     it('sets the network div within the dapp', async () => {
       await driver.switchTo().window(dapp)
-      const networkDiv = await findElement(driver, By.css('#network'))
+      const networkDiv = await findElement(driver, By.css('#networkDiv'))
       assert.equal(await networkDiv.getText(), '3')
     })
 
     it('sets the chainId div within the dapp', async () => {
       await driver.switchTo().window(dapp)
-      const chainIdDiv = await findElement(driver, By.css('#chainId'))
+      const chainIdDiv = await findElement(driver, By.css('#chainIdDiv'))
       assert.equal(await chainIdDiv.getText(), '0x3')
     })
 
     it('sets the account div within the dapp', async () => {
       await driver.switchTo().window(dapp)
-      const accountsDiv = await findElement(driver, By.css('#accounts'))
+      const accountsDiv = await findElement(driver, By.css('#accountsDiv'))
       assert.equal(await accountsDiv.getText(), publicAddress.toLowerCase())
     })
   })
